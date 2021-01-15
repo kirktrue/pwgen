9a3235ef1facd532e4e3ac789649a58b05eae077
Kirk True <kirk@mustardgrain.com>
2021-01-14 16:55:51 -0800

    Testing and refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,69 @@
-use std::error::Error;
 use rand::Rng;
+use std::error::Error;
+use std::process;
 use structopt::StructOpt;
 
+const MIN_LENGTH: usize = 8;
+
 #[derive(Debug, StructOpt)]
-#[structopt(about = "Download binaries.")]
-pub struct App {
-    #[structopt(about = "Maximum length of the password", default_value = "64", short = "m", long = "max-length")]
+#[structopt(about = "Generates passwords from a set of the most common English words.")]
+pub struct Args {
+    #[structopt(
+        help = "Maximum length of the password",
+        default_value = "32",
+        short = "m",
+        long = "max-length"
+    )]
     pub max_len: usize,
-    #[structopt(about = "Specify for verbosity", short = "v", long = "verbose")]
+    #[structopt(help = "Specify for verbosity", short = "v", long = "verbose")]
     pub verbose: bool,
-    #[structopt(about = "Do not print the trailing newline character", short = "n", long = "newline-omit")]
+    #[structopt(
+        help = "Do not print (omit) the trailing newline character",
+        short = "n",
+        long = "newline-omit"
+    )]
     pub omit_newline: bool,
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let app = App::from_args();
+    let args = Args::from_args();
+
+    if args.max_len < MIN_LENGTH {
+        eprintln!("Please ensure -m/--max-length is >= {}.", MIN_LENGTH);
+        process::exit(1);
+    }
+
     let bytes = include_bytes!("../words.txt");
     let words = String::from_utf8_lossy(bytes);
     let lines: Vec<&str> = words.lines().collect();
 
-    // let max_len = 16;
-    // let verbose = false;
-
-    let max_words = 4;
-    let max_tries = 20;
-    let mut available_len = app.max_len - 2; // max_words is also number of trailing dashes, 2 is the digit suffix
+    let mut available_len = args.max_len - 2; // max_words is also number of trailing dashes, 2 is the digit suffix
 
     let mut rando = rand::thread_rng();
-    let mut words= vec![];
+    let mut words = vec![];
 
-    for _i in 1..max_tries {
+    loop {
         let index = rando.gen_range(0, lines.len());
         let word = lines[index];
 
-        if app.verbose {
+        if args.verbose {
             println!("available_len: {}, candidate word: {}", available_len, word);
         }
 
         let word_len = word.len();
 
         if word_len > available_len {
-            continue
+            continue;
         }
 
         words.push(word);
         available_len -= word_len;
 
-        if app.verbose {
+        if args.verbose {
             println!("Remaining available_len: {}", available_len);
         }
 
-        if words.len() >= max_words || available_len <= 4 {
+        if available_len <= 4 {
             break;
         }
     }
@@ -58,7 +71,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut pwd = String::new();
 
     for (i, word) in words.iter().enumerate() {
-        if app.verbose {
+        if args.verbose {
             println!("Element at position {}: {:?}", i, word);
         }
 
@@ -66,7 +79,7 @@ fn main() -> Result<(), Box<dyn Error>> {
             let first_letter = &word[0..1].to_uppercase();
             let rest_of_letters = &word[1..];
 
-            if app.verbose {
+            if args.verbose {
                 println!("first_letter: {}", first_letter);
                 println!("rest_of_letters: {}", rest_of_letters);
             }
@@ -85,13 +98,13 @@ fn main() -> Result<(), Box<dyn Error>> {
         suffix = rando.gen_range(0, 10);
 
         if suffix > 0 {
-            break
+            break;
         }
     }
 
     pwd.push_str(format!("-{:02}", suffix).as_str());
 
-    if app.omit_newline{
+    if args.omit_newline {
         print!("{}", pwd);
     } else {
         println!("{}", pwd);
