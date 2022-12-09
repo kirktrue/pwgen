87313fb3983c64cec8534c64aba7accd77ea1d09
Kirk True <kirk@kirktrue.pro>
2022-12-08 21:00:24 -0800

    Refactoring

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,67 @@ use structopt::StructOpt;
 
 const MIN_LENGTH: usize = 8;
 
+struct Pwgen {
+    lines: Vec<String>,
+}
+
+impl Pwgen {
+    pub fn generate_password(self, max_len: usize, verbose: bool) -> String {
+        let mut pwd = String::new();
+        let mut rando = rand::thread_rng();
+
+        loop {
+            let index = rando.gen_range(0..self.lines.len()) as usize;
+            let word = self.lines[index].clone();
+            let token_len = word.len() + 1;
+
+            if verbose {
+                println!("pwd: {}, word: {}, token_len: {}", pwd, word, token_len);
+            }
+
+            // The extra two characters we reserve here are for the two-digit digit suffix.
+            if token_len > (max_len - pwd.len() - 2) {
+                println!("max_len: {}, pwd (len): {}, token_len: {}, continuing...", max_len, pwd.len(), token_len);
+                continue;
+            }
+
+            if pwd.len() == 0 {
+                pwd.push_str(&word[0..1].to_uppercase());
+                pwd.push_str(&word[1..]);
+            } else {
+                pwd.push_str(word.as_str());
+            }
+
+            pwd.push('-');
+
+            if verbose {
+                println!("max_len: {}, pwd: {}, word: {}, token_len: {}", max_len, pwd, word, token_len);
+            }
+
+            if max_len - pwd.len() <= 2 {
+                println!("max_len: {}, pwd (len): {}, breaking...", max_len, pwd.len());
+                break;
+            }
+        }
+
+        loop {
+            let range = 0..10;
+            let suffix:u32 = rando.gen_range(range);
+
+            if suffix > 0 {
+                pwd.push_str(format!("{:02}", suffix).as_str());
+                break;
+            }
+        }
+
+        if verbose {
+            println!("pwd: {}", pwd);
+        }
+
+        pwd
+    }
+}
+
 #[derive(Debug, StructOpt)]
 #[structopt(about = "Generates passwords from a set of the most common English words.")]
 pub struct Args {
@@ -35,65 +96,10 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let bytes = include_bytes!("../words.txt");
     let words = String::from_utf8_lossy(bytes);
-    let lines: Vec<&str> = words.lines().collect();
+    let lines = words.lines().map(|s| s.to_owned()).collect();
 
-    let mut available_len = args.max_len - 2; // max_words is also number of trailing dashes, 2 is the digit suffix
-
-    let mut rng = rand::thread_rng();
-    let mut words = vec![];
-
-    loop {
-        let index = rng.gen_range(0, lines.len());
-        let word = lines[index];
-
-        if args.verbose {
-            println!("available_len: {}, candidate word: {}", available_len, word);
-        }
-
-        let word_len = word.len();
-
-        if word_len > available_len {
-            continue;
-        }
-
-        words.push(word);
-        available_len -= word_len;
-
-        if args.verbose {
-            println!("Remaining available_len: {}", available_len);
-        }
-
-        if available_len <= 4 {
-            break;
-        }
-    }
-
-    let mut pwd = String::new();
-
-    for (i, word) in words.iter().enumerate() {
-        if args.verbose {
-            println!("Element at position {}: {:?}", i, word);
-        }
-
-        if i == 0 {
-            let first_letter = &word[0..1].to_uppercase();
-            let rest_of_letters = &word[1..];
-
-            if args.verbose {
-                println!("first_letter: {}", first_letter);
-                println!("rest_of_letters: {}", rest_of_letters);
-            }
-
-            pwd.push_str(first_letter);
-            pwd.push_str(rest_of_letters);
-        } else {
-            pwd.push('-');
-            pwd.push_str(word);
-        }
-    }
-
-    let suffix = rng.gen_range(1, 100);
-    pwd.push_str(format!("-{:02}", suffix).as_str());
+    let pwgen = Pwgen{lines};
+    let pwd = pwgen.generate_password(args.max_len, args.verbose);
 
     if args.omit_newline {
         print!("{}", pwd);
@@ -102,4 +108,23 @@ fn main() -> Result<(), Box<dyn Error>> {
     }
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::Pwgen;
+
+    fn init() -> Pwgen {
+        let bytes = include_bytes!("../words.txt");
+        let words = String::from_utf8_lossy(bytes);
+        let lines = words.lines().map(|s| s.to_string()).collect();
+        Pwgen { lines }
+    }
+
+    #[test]
+    pub fn test() {
+        let pwgen = init();
+        let password = pwgen.generate_password(32, true);
+        assert_eq!(32, password.len());
+    }
 }
